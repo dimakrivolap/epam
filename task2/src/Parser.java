aed51a300015e94af95d9c74e637746574e5ed59
dimakrivolap <krivolap.dima@gmail.com>
2018-07-16 17:01:06 +0300

    Task2 regexp

---
+++ task2/src/Parser.java
@@ -0,0 +1,52 @@
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Parser {
+    private String text;
+    public Parser(String file) {
+        while (true) {
+            try {
+                FileReader fr = new FileReader(file);
+                Scanner scan = new Scanner(fr);
+                int i = 1;
+                while (scan.hasNextLine()) {
+                    text+=scan.nextLine();
+                    i++;
+                }
+                fr.close();
+            }
+            catch (Exception e){
+
+            }
+
+        }
+    }
+
+    private List<String> getSentences(){
+        Matcher matcher = Pattern.compile("([^.!?]+)").matcher(text);
+        List<String> list = new ArrayList<>();
+        while (matcher.find())
+        {
+            list.add(matcher.group(1));
+        }
+        return list;
+    }
+
+    private String[] getWords(String sentence){
+        Pattern pattern = Pattern.compile("[ ,.!?]");
+        return pattern.split(sentence);
+    }
+
+    private void getSizeSentences(){
+        List<String> sentences = getSentences();
+        for (String sentence : sentences){
+            String[] words = getWords(sentence);
+
+
+        }
+    }
+}
